d745b08d914d10124f5ba807e6eb6076e1d8c67f
Shaivik Semwal <shaiviksemwal@gmail.com>
2023-06-20 13:03:18 +0530

    get account by prefix

--- experiment.ts
+++ experiment.ts
@@ -1,18 +1,33 @@
-import { StargateClient, Block } from "@cosmjs/stargate"
+import { Block, SigningStargateClient} from "@cosmjs/stargate"
+import { DirectSecp256k1HdWallet} from "@cosmjs/proto-signing"
+import { encodeSecp256k1Pubkey, pubkeyToAddress } from '@cosmjs/amino';
 
 const rpc = "localhost:26657"
+const mnemonic = "damp dish bottom grain swamp view merge noise lunar math above old all now diamond pact boy flavor you express awake color token salt";
+const addressPrefix = 'cosmos';
 
 const runAll = async(): Promise<void> => {
+    
+    // create wallet and get account
+    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic);
+    const [account] = await wallet.getAccounts();
+    const signer = wallet;
+
     console.log("enter");
     
-    const client = await StargateClient.connect(rpc)
-    console.log("With client, chain id:", await client.getChainId(), ", height:", await client.getHeight())
+    // creating connection instance
+    const client = await SigningStargateClient.connectWithSigner(rpc, signer);
 
-    // console.log("Alice balances:", await client.getAllBalances("cosmos1lkvnypzp9833lksyq3jur45ll5xq5vmlvk32gh"), )
-    
+    // get address with prefix
+    const publicKey = encodeSecp256k1Pubkey(account.pubkey);
+    const address = pubkeyToAddress(publicKey, addressPrefix);
+    console.log(address)
+    console.log("With client, chain id:", await client.getChainId(), ", height:", await client.getHeight())
+    console.log("Alice balances:", await client.getAllBalances("cosmos1lkvnypzp9833lksyq3jur45ll5xq5vmlvk32gh"), )
+
+    // get block data
     const blockHeight = await client.getHeight(); // Replace with the desired block height
     const block: Block | null = await client.getBlock(blockHeight);
-
     if (block) {
       console.log('Block Data:', block);
     } else {
