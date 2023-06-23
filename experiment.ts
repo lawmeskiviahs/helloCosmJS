27f69b7946a15914bcc1f17b0c6c24c37c7b9a41
Shaivik Semwal <shaiviksemwal@gmail.com>
2023-06-23 14:48:35 +0530

    code structure improved

--- experiment.ts
+++ experiment.ts
@@ -1,22 +1,23 @@
 import { SigningCosmWasmClient } from "@cosmjs/cosmwasm-stargate";
-import { GasPrice, calculateFee } from "@cosmjs/stargate"
+import { GasPrice, calculateFee, QueryClient } from "@cosmjs/stargate"
 import { Tendermint37Client } from "@cosmjs/tendermint-rpc";
 import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing"
 
 class CosmJsRpcMethods {
-    private mnemonic = "antenna name board hidden shove path team betray blur lens torch sand level laundry family manage virus lottery loan cruise rough basic panther bring";
-    private contract_address="wasm14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9s0phg4d"
-    private client: any;
-    private tendermintClient: any;
-    private rpUrl:string = "http://localhost:26657";
+    public mnemonic = "antenna name board hidden shove path team betray blur lens torch sand level laundry family manage virus lottery loan cruise rough basic panther bring";
+    public contract_address="wasm14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9s0phg4d"
+    public cosmWasmClient: any;
+    public tendermintClient: any;
+    public queryClient: any;
+    public rpUrl:string = "http://localhost:26657";
     // private txHash:string = "9D79E53956FAD1A04CA92EE2A585FF1D40F6C6FFBF5C7F0B098AB2D2FAE49EE8";
-    private txHash:string = "0C6D0A73464F29A9E74F64344784CDA8A76B993EBE252F5F89FB75915AE418D1";
-    private blockNumber:number = parseInt("64");
+    public txHash:string = "0C6D0A73464F29A9E74F64344784CDA8A76B993EBE252F5F89FB75915AE418D1";
+    public blockNumber:number = parseInt("0");
 
     public async getTransaction() {
         try {
-            this.client = await SigningCosmWasmClient.connect(this.rpUrl);
-            const response = await this.client.queryClient.tx.getTx(this.txHash)
+            this.cosmWasmClient = await SigningCosmWasmClient.connect(this.rpUrl);
+            const response = await this.cosmWasmClient.queryClient.tx.getTx(this.txHash)
             return response;
         } catch (err) {
             console.log("errrorr==", err);
@@ -26,8 +27,11 @@ class CosmJsRpcMethods {
 
     public async getBlockData() {
         try {
-            this.client = await SigningCosmWasmClient.connect(this.rpUrl);
-            const response = await this.client.getBlock(await this.client.getHeight());
+            const wallet = await DirectSecp256k1HdWallet.fromMnemonic(this.mnemonic, { prefix: 'wasm'});
+            this.tendermintClient = await Tendermint37Client.connect(this.rpUrl);
+            this.cosmWasmClient = await SigningCosmWasmClient.createWithSigner(this.tendermintClient, wallet);
+            this.blockNumber = await this.cosmWasmClient.getHeight();
+            const response = await this.cosmWasmClient.getBlock(this.blockNumber);
             return response;
         } catch (err) {
             console.log("errrorr==", err);
@@ -37,8 +41,11 @@ class CosmJsRpcMethods {
 
     public async getFullBlockInfo() {
         try {
-            this.client = await SigningCosmWasmClient.connect(this.rpUrl);
-            const response = await this.client.queryClient.tmClient.block(await this.client.getHeight())
+            const wallet = await DirectSecp256k1HdWallet.fromMnemonic(this.mnemonic, { prefix: 'wasm'});
+            this.tendermintClient = await Tendermint37Client.connect(this.rpUrl);
+            this.cosmWasmClient = await SigningCosmWasmClient.createWithSigner(this.tendermintClient, wallet);
+            this.blockNumber = await this.cosmWasmClient.getHeight();
+            const response = await this.cosmWasmClient.queryClient.tmClient.block(this.blockNumber)
             return response;
         } catch (err) {
             console.log("errrorr==", err);
@@ -59,7 +66,10 @@ class CosmJsRpcMethods {
 
     public async getBlockRewards() {
         try {
+            const wallet = await DirectSecp256k1HdWallet.fromMnemonic(this.mnemonic, { prefix: 'wasm'});
             this.tendermintClient = await Tendermint37Client.connect(this.rpUrl);
+            this.cosmWasmClient = await SigningCosmWasmClient.createWithSigner(this.tendermintClient, wallet);
+            this.blockNumber = await this.cosmWasmClient.getHeight();
             const response = await this.tendermintClient.blockResults(this.blockNumber);
             return response;
         } catch (err) {
@@ -90,15 +100,14 @@ class CosmJsRpcMethods {
         }
     }
 
-    public async mint() {
+    public async mint(amount:string) {
         try {
             const wallet = await DirectSecp256k1HdWallet.fromMnemonic(this.mnemonic, { prefix: 'wasm'});
             const address = (await wallet.getAccounts())[0].address
             const wrongAddress = 'wasm1art52u07fznl55xjttfz8negdegekluwj90dzh'
             const tmClient = await Tendermint37Client.connect(this.rpUrl);
-            console.log(address);
-            console.log(this.contract_address);
-            
+            // console.log(address);
+            // console.log(this.contract_address);
             
             const gasPrice = GasPrice.fromString('1000stake');
             // console.log(gasPrice);
@@ -106,16 +115,16 @@ class CosmJsRpcMethods {
             const executeFee = calculateFee(3508879, gasPrice);
             // console.log(executeFee);
             
-            this.client = await SigningCosmWasmClient.createWithSigner(tmClient, wallet);
+            this.cosmWasmClient = await SigningCosmWasmClient.createWithSigner(tmClient, wallet);
             // console.log(this.client);
             
-            const response = await this.client.execute(
+            const response = await this.cosmWasmClient.execute(
                address, 
                this.contract_address,
                {
                     mint:{
                     recipient: "wasm1art52u07fznl55xjttfz8negdegekluwj90dzh",
-                    amount: '10',
+                    amount: amount, 
                     },
                } ,
             executeFee
@@ -138,18 +147,18 @@ const methods = new CosmJsRpcMethods();
     // const blockData = await methods.getBlockData();
     // const fullblockInfo = await methods.getFullBlockInfo();
     // const validators = await methods.getAllBlockValidator();
-    const rewards = await methods.getBlockRewards();
+    // const rewards = await methods.getBlockRewards();
     // const health = await methods.getHealth();
     // const status = await methods.getStatus();
-    const mintResp = await methods.mint();
+    // const mintResp = await methods.mint();
     // console.log("transaction Data============", transactionData);
     // console.log("Block Data============", blockData);
     // console.log("Full Block Data==========", fullblockInfo);
     // console.log("All Validators=========", validators);
-    console.log("Block Rewards=========", rewards);
+    // console.log("Block Rewards=========", rewards);
     // console.log("Node Health=========", health);
     // console.log("Node Status=========", status);
-    console.log("mintResp=================", mintResp);
+    // console.log("mintResp=================", mintResp);
     
 })();
 
--- listner.ts
+++ listner.ts
@@ -1,16 +1,15 @@
 import WebSocket from "websocket"
 import CosmJsRpcMethods from './experiment'
+import { log } from "console";
 const contract_address="wasm14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9s0phg4d"
 
 
-function connect(){
+async function connect(){
 // Creates new WebSocket object with a wss URI as the parameter
 const socket = new WebSocket.w3cwebsocket('ws://localhost:26657/websocket');
 
-
-
 // Fired when a connection with a WebSocket is opened
-socket.onopen = function () {
+socket.onopen = async function () {
     socket.send(JSON.stringify({ "jsonrpc": "2.0", "method": "subscribe", "params": ["tm.event='NewBlock'"], "id": 1 }))
     //NewBlock
     //Tx
@@ -19,32 +18,37 @@ socket.onopen = function () {
 };
 
 // Fired when data is received through a WebSocket
+socket.onmessage = async function (event:any) {
 
-socket.onmessage = function (event:any) {
 
     try {
-        const data=JSON.parse(event.data).result;
-        CosmJsRpcMethods.getBlockRewards
-        console.log(data);
-        
+        // const data=JSON.parse(event.data).result;
+        const blockRewards = await CosmJsRpcMethods.getBlockRewards();
+        console.log(blockRewards);
+        // const blockData = await CosmJsRpcMethods.getBlockData();
+        // console.log(blockData);
+        // const blockFullData = await CosmJsRpcMethods.getFullBlockInfo();
+        // console.log(blockFullData);
+        // const amount:string = "1";
+        // const mintResp = await CosmJsRpcMethods.mint(amount);
+        // console.log(mintResp);
     }
        catch (error:any) {
-        console.log(error.message);
-        
+        console.log(error.message);  
     }
-
 
 };
 
 // Fired when a connection with a WebSocket is closed
-socket.onclose = function () {
+socket.onclose = async function () {
 console.log('Socket is closed. Reconnect will be attempted in 1 second.');
 setTimeout(function() {
   connect();
 }, 1000);
 }
+
 // Fired when a connection with a WebSocket has been closed because of an error
-socket.onerror = function (event:any) {
+socket.onerror = async function (event:any) {
     console.log("connection error", event)
 };
 }
